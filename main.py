97155256292ed4696e24a34304f92e454e72fe14
Markmk12 <75953003+Markmk12@users.noreply.github.com>
2023-09-22 02:47:15 +0300

    Added normed network efficiency

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# Main program
-
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -7,6 +5,27 @@ from traffic import function_lib as tf
 from pavement import function_lib as pv
 from network_efficiency import function_lib as eff
 
+# Notes and TODOs:
+# Note 1
+# Note 1
+# ...
+
+# Ideal road network
+road_network_0 = nx.Graph()
+road_network_0.add_node(1)
+road_network_0.add_node(2)
+road_network_0.add_node(3)
+road_network_0.add_node(4)
+road_network_0.add_node(5)
+road_network_0.add_edge(1, 2, key=0, length=100, lanes=4, velocity=100, AAT=450, PCI=100, time=60)   # Der key-Parameter hilft dabei, die verschiedenen Kanten zu unterscheiden.
+road_network_0.add_edge(2, 1, key=1, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+road_network_0.add_edge(2, 3, key=2, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+road_network_0.add_edge(1, 3, key=3, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+road_network_0.add_edge(3, 4, key=4, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+road_network_0.add_edge(2, 4, key=5, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+road_network_0.add_edge(4, 5, key=6, length=100, lanes=4, velocity=100, AAT=700, PCI=100, time=60)
+
+# Road network for simulation
 road_network_1 = nx.Graph()
 road_network_1.add_node(1)
 road_network_1.add_node(2)
@@ -25,13 +44,14 @@ road_network_1.add_edge(4, 5, key=6, length=100, lanes=4, velocity=100, AAT=700,
 nx.draw(road_network_1)
 plt.show()
 
-# Alle Kanten mit ihren Attributen als Liste ausgeben
+
+# All edges with attributes as list
 # road_list_1 = list(road_network_1.edges(data=True, keys=True))
 # print(road_list_1)
 
+
 # Define PCI groups
 PCI_groups = np.array([100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0])
-# PCI_groups = np.array(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'])
 
 
 # Define a Probability Transition Matrix and initial vector
@@ -49,13 +69,14 @@ transition_matrix = np.array([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 ])
 
+# Initial status PCI = 100
 initial_status = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
 
 PCI_history = []
 velocity_history = []
 travel_time_history = []
-efficiency_history = []
+normed_efficiency_history = []
 
 # Simulation of the network for 100 years
 for j in range(100):
@@ -69,39 +90,40 @@ for j in range(100):
     velocity_history.append(road_network_1[1][2]['velocity'])
     travel_time_history.append(road_network_1[1][2]['time'])
 
-    # Network Efficiency
+    # Normed network Efficiency at time t
     efficiency_t = eff.network_efficiency(road_network_1)
+    normed_efficiency_t = efficiency_t / eff.network_efficiency(road_network_0)
 
-    # Saving network efficiency history data
-    efficiency_history.append(efficiency_t)
+    # Saving network efficiency prediction data
+    normed_efficiency_history.append(normed_efficiency_t)
 
 # Create a figure and axes for the subplots in a 2x2 layout
 fig, axs = plt.subplots(2, 2, figsize=(12, 12))
 
-# Plot for PCI history for edge (1, 2)
+# Plot for PCI prediction for edge (1, 2)
 axs[0, 0].plot(range(100), PCI_history, color='tab:red')
 axs[0, 0].set_ylabel('PCI')
 axs[0, 0].set_title("PCI prediction for edge (1, 2)")
 axs[0, 0].grid(True)
 
-# Plot for Velocity history for edge (1, 2)
+# Plot for Velocity prediction for edge (1, 2)
 axs[0, 1].plot(range(100), velocity_history, color='tab:red')
 axs[0, 1].set_ylabel('Velocity [km/h]')
 axs[0, 1].set_title("Velocity prediction for edge (1, 2)")
 axs[0, 1].grid(True)
 
-# Plot for Travel Time history for edge (1, 2)
+# Plot for Travel Time prediction for edge (1, 2)
 axs[1, 0].plot(range(100), travel_time_history, color='tab:red')
 axs[1, 0].set_xlabel('Year')
 axs[1, 0].set_ylabel('Travel time [min]')
 axs[1, 0].set_title("Travel time prediction for edge (1, 2)")
 axs[1, 0].grid(True)
 
-# Plot for Efficiency History
-axs[1, 1].plot(range(100), efficiency_history, color='tab:red')
+# Plot for efficiency prediction
+axs[1, 1].plot(range(100), normed_efficiency_history, color='tab:red')
 axs[1, 1].set_xlabel('Year')
-axs[1, 1].set_ylabel('Network Efficiency [-]')
-axs[1, 1].set_title("Network efficiency prediction")
+axs[1, 1].set_ylabel('Normed network Efficiency [-]')
+axs[1, 1].set_title("Normed network efficiency prediction")
 axs[1, 1].grid(True)
 
 # Display the plots
@@ -113,5 +135,10 @@ plt.show()
 road_list_1 = list(road_network_1.edges(data=True))
 print(road_list_1)
 
+# Network efficiency
 efficiency = eff.network_efficiency(road_network_1)
 print(efficiency)
+
+# Normalized efficiency
+norm_efficiency = efficiency / eff.network_efficiency(road_network_0)
+print(norm_efficiency)
--- pavement/function_lib.py
+++ pavement/function_lib.py
@@ -4,10 +4,10 @@ import numpy as np
 def pavement_deterioration(pci, pci_groups, transition_matrix, status, t):
 
     # Markov Chain after t time steps
-    p_distribution = status.dot(np.linalg.matrix_power(transition_matrix, t))                # probability distribution at t
+    p_distribution = status.dot(np.linalg.matrix_power(transition_matrix, t))           # probability distribution at t
 
     # MC samples
-    sample = np.random.choice(pci_groups, 100000, p=p_distribution)
+    sample = np.random.choice(pci_groups, 100000, p=p_distribution)            # best results when using >10,000 samples
 
     # Expected value (arithmetic mean)
     mean = np.mean(sample)
