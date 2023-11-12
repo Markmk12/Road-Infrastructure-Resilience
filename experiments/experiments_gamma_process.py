4bde2347d2f3c2d37775d807ea97a3acf0bea113
mark <markmk.alt@gmail.com>
2023-11-12 03:39:43 +0100

    Update

--- experiments/experiments_gamma_process.py
+++ experiments/experiments_gamma_process.py
@@ -5,12 +5,12 @@ import os
 # Disable LaTeX in plots (use the built-in Matplotlib renderer)
 plt.rcParams['text.usetex'] = False
 
-# Environmental deterioration
-alpha_environment = 0.1           # 1
+# Environmental deterioration           # traffic ???
+alpha_environment = 1           # 1
 beta_environment = 1            # 1
 
-# Deterioration through traffic
-alpha_traffic = 0.05         # 0.5
+# Deterioration through traffic         # environment ???
+alpha_traffic = 0.5         # 0.5
 beta_traffic = 0.3           # 0.3
 
 # Weight of the degradation
@@ -43,20 +43,20 @@ cum_Y = np.cumsum(Y_values)
 cum_Y_weighted = np.cumsum(Y_weighted_values)
 
 # Plots
-plt.step(interval, cum_X_1, label=r'$X_1(t)$')
-plt.step(interval, cum_X_2, label=r'$X_2(t)$')
+plt.step(interval, cum_X_1, label=r'$X_{traffic}(t)$')
+plt.step(interval, cum_X_2, label=r'$X_{environment}(t)$')
 plt.step(interval, cum_Y, label=r'$Y_{unweighted}(t)$')
-plt.step(interval, cum_Y_weighted, label=r'$Y(t)$')
+plt.step(interval, cum_Y_weighted, label=r'$Y_{weighted}(t)$')
 
 # plt.title('Gamma Processes')
 plt.xlabel('t')
-plt.ylabel('Degradation')  # Using built-in Matplotlib renderer for y label
+plt.ylabel('Pavement degradation')  # Using built-in Matplotlib renderer for y label
 plt.legend()
 
 # Saving the plots in different file formats
-plt.savefig("gamma_3.png")
-plt.savefig("gamma_3.svg")
-plt.savefig("gamma_3.eps")  # no transparency
+plt.savefig("gamma_4.png")
+plt.savefig("gamma_4.svg")
+plt.savefig("gamma_4.eps")  # no transparency
 
 # Plot show
 plt.tight_layout()
